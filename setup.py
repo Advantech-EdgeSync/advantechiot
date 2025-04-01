5d4b00a0063783157791e06e73d3e84af04771e4
likevintw <likevintw@gmail.com>
2025-04-01 14:50:12 +0800

    install good to go

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ from setuptools import setup
 
 setup(
     name="advantechiot",
-    version="0.1.54",
+    version="0.1.55",
     author='Keng Wei Li',
     author_email = 'Kengwei.Li@advantech.com.tw',
     packages=["advantechiot"],
     package_data={
         "advantechiot": [
-            "Driver_x86/*.sh",
-            "Driver_arm/*.sh",
-            "Driver_x86/*.so",
-            "Driver_arm/*.so",
-            "Driver_x86/*.ini",
-            "Driver_arm/*.ini",
-            "Driver_x86/AI/service/*",
+            "Driver_x86/*",
+            "Driver_arm/*",
         ],
     },
     include_package_data=True,
